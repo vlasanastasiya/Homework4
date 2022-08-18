127368b192d51dc6568d4a1b0f5d1d3bf4e2713a
vlasenkova <asyicheva1@mail.ru>
2022-08-18 17:29:30 +0300

    second dz

---
+++ zadacha27.md/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+void Zadacha27()
+{
+       Console.WriteLine("Введите число");
+       int number = Convert.ToInt32(Console.ReadLine());
+       Console.WriteLine($"Сумма цифр в числе {number} равна {Findsum(number)}");
+}
+int Findsum(int number)
+{ 
+     int sum = 0;
+     while(number > 0) 
+{
+     sum = sum + number%10;  
+     number = number/10;
+}
+return sum;
+}
+Zadacha27();
